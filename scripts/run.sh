31c054a46b2ef405af01df4a959471db39bbeff9
kingspp <kingspprathyush@gmail.com>
2016-01-07 11:23:31 +0530

    Modified run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,14 @@
+#!/bin/bash 
+command -v aria2c >/dev/null 2>&1 || { echo "Installing Aria2 Library" >&2; sudo apt-get -y install aria2;}
+
 aria2c --enable-rpc --rpc-listen-all &
-mvn -f ../pom.xml clean compile assembly:single
-java -jar ../target/app-jar-with-dependencies.jar &
+if [ ! -f ../target/DownloadManager-jar-with-dependencies.jar ]; then
+    echo "Jar not Found, Running Build. . ." &
+    mvn -f ../pom.xml clean compile ass!!embly:single
+fi
+java -jar ../target/DownloadManager-jar-with-dependencies.jar &
 sleep 5
 
 # For Macintosh
-open http://localhost:9999
+#open http://localhost:9999
+#To do.. check for all environments and run actions based on environments
